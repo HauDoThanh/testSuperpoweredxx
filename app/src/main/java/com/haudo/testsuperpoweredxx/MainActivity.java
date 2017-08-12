f3086a8e5feb1279e9ab8d45c13be49a39ca455b
admin <trangnguyenchuoi@gmail.com>
2017-08-12 16:13:13 +0700

    Initial commit

---
+++ app/src/main/java/com/haudo/testsuperpoweredxx/MainActivity.java
@@ -0,0 +1,157 @@
+package com.haudo.testsuperpoweredxx;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    boolean playing = false;
+    boolean open = false;
+
+    static int REVERB_NON = 0;
+    static int REVERB_DRY = 1;
+    static int REVERB_WET = 2;
+    static int REVERB_WIDTH = 3;
+    static int REVERB_MIX = 4;
+    static int REVERB_ROOMSIZE = 5;
+    static int REVERB_DAMP = 6;
+
+    private TextView txtDry, txtWidth, txtRoomSize, txtMix, txtWet, txtDamp, txtEchoValue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get the device's sample rate and buffer size to enable low-latency Android audio output, if available.
+        String samplerateString = null, buffersizeString = null;
+        if (Build.VERSION.SDK_INT >= 17) {
+            AudioManager audioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+            samplerateString = audioManager.getProperty(AudioManager.PROPERTY_OUTPUT_SAMPLE_RATE);
+            buffersizeString = audioManager.getProperty(AudioManager.PROPERTY_OUTPUT_FRAMES_PER_BUFFER);
+        }
+        if (samplerateString == null) samplerateString = "44100";
+        if (buffersizeString == null) buffersizeString = "512";
+
+        // Files under res/raw are not zipped, just copied into the APK. Get the offset and length to know where our files are located.
+        AssetFileDescriptor fd0 = getResources().openRawResourceFd(R.raw.moc),
+                fd1 = getResources().openRawResourceFd(R.raw.beat);
+        int fileAoffset = (int) fd0.getStartOffset(), fileAlength = (int) fd0.getLength(), fileBoffset = (int) fd1.getStartOffset(), fileBlength = (int) fd1.getLength();
+
+        try {
+            fd0.getParcelFileDescriptor().close();
+            fd1.getParcelFileDescriptor().close();
+        } catch (IOException e) {
+            android.util.Log.d("", "Close error.");
+        }
+
+
+        // Arguments: path to the APK file, offset and length of the two resource files, sample rate, audio buffer size.
+        SuperpoweredExample(Integer.parseInt(samplerateString), Integer.parseInt(buffersizeString), getPackageResourcePath(), fileAoffset, fileAlength, fileBoffset, fileBlength);
+
+
+        // reverb events
+        final SeekBar fxDry = (SeekBar) findViewById(R.id.reverb_dry);
+        fxDry.setOnSeekBarChangeListener(seekBarChangeListener);
+        final SeekBar fxWidth = (SeekBar) findViewById(R.id.reverb_width);
+        fxWidth.setOnSeekBarChangeListener(seekBarChangeListener);
+        final SeekBar fxMix = (SeekBar) findViewById(R.id.reverb_mix);
+        fxMix.setOnSeekBarChangeListener(seekBarChangeListener);
+        final SeekBar fxWet = (SeekBar) findViewById(R.id.reverb_wet);
+        fxWet.setOnSeekBarChangeListener(seekBarChangeListener);
+        final SeekBar fxRoomSize = (SeekBar) findViewById(R.id.reverb_roomsize);
+        fxRoomSize.setOnSeekBarChangeListener(seekBarChangeListener);
+        final SeekBar fxDamp = (SeekBar) findViewById(R.id.reverb_damp);
+        fxDamp.setOnSeekBarChangeListener(seekBarChangeListener);
+
+        final SeekBar fxMixEcho = (SeekBar) findViewById(R.id.echo_mix);
+        fxMixEcho.setOnSeekBarChangeListener(seekBarChangeListener);
+
+        txtDry = (TextView) findViewById(R.id.dry_value);
+        txtMix = (TextView) findViewById(R.id.mix_value);
+        txtRoomSize = (TextView) findViewById(R.id.roomsize_value);
+        txtWet = (TextView) findViewById(R.id.wet_value);
+        txtWidth = (TextView) findViewById(R.id.width_value);
+        txtDamp = (TextView) findViewById(R.id.damp_value);
+        txtEchoValue = (TextView) findViewById(R.id.echo_value);
+
+    }
+
+    private OnSeekBarChangeListener seekBarChangeListener = new OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            int id = seekBar.getId();
+            String value = String.valueOf(progress * 0.01);
+            switch (id) {
+                case R.id.reverb_dry:
+                    onFxReverbValue(REVERB_DRY, progress);
+                    txtDry.setText(value);
+                    break;
+                case R.id.reverb_mix:
+                    onFxReverbValue(REVERB_MIX, progress);
+                    txtMix.setText(value);
+                    break;
+                case R.id.reverb_roomsize:
+                    onFxReverbValue(REVERB_ROOMSIZE, progress);
+                    txtRoomSize.setText(value);
+                    break;
+                case R.id.reverb_wet:
+                    onFxReverbValue(REVERB_WET, progress);
+                    txtWet.setText(value);
+                    break;
+                case R.id.reverb_width:
+                    onFxReverbValue(REVERB_WIDTH, progress);
+                    txtWidth.setText(value);
+                    break;
+                case R.id.reverb_damp:
+                    onFxReverbValue(REVERB_DAMP, progress);
+                    txtDamp.setText(value);
+                    break;
+                case R.id.echo_mix:
+                    onEcho(progress);
+                    txtEchoValue.setText(value);
+                    break;
+            }
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
+
+    public void SuperpoweredExample_PlayPause(View button) {  // Play/pause.
+        playing = !playing;
+        onPlayPause(playing);
+        Button b = (Button) findViewById(R.id.playPause);
+        if (b != null) b.setText(playing ? "Pause" : "Play");
+    }
+
+    private native void SuperpoweredExample(int samplerate, int buffersize, String apkPath, int fileAoffset, int fileAlength, int fileBoffset, int fileBlength);
+
+    private native void onPlayPause(boolean play);
+
+    private native void onFxReverbValue(int reverb_type, int value);
+
+    private native void onEcho(int mix);
+
+    static {
+        System.loadLibrary("SuperpoweredExample");
+    }
+}
---
+++ app/src/main/jni/SuperpoweredExample.cpp
@@ -0,0 +1,191 @@
+#include "SuperpoweredExample.h"
+#include <SuperpoweredSimple.h>
+#include <SuperpoweredCPU.h>
+#include <android/log.h>
+#include <SLES/OpenSLES.h>
+#include <SLES/OpenSLES_AndroidConfiguration.h>
+#include <malloc.h>
+
+static void playerEventCallbackA(void *clientData, SuperpoweredAdvancedAudioPlayerEvent event,
+                                 void *__unused value) {
+    if (event == SuperpoweredAdvancedAudioPlayerEvent_LoadSuccess) {
+        SuperpoweredAdvancedAudioPlayer *playerA = *((SuperpoweredAdvancedAudioPlayer **) clientData);
+        playerA->setBpm(126.0f);
+        playerA->setFirstBeatMs(353);
+        playerA->setPosition(playerA->firstBeatMs, false, false);
+    };
+}
+
+static void playerEventCallbackB(void *clientData, SuperpoweredAdvancedAudioPlayerEvent event,
+                                 void *__unused value) {
+    if (event == SuperpoweredAdvancedAudioPlayerEvent_LoadSuccess) {
+        SuperpoweredAdvancedAudioPlayer *playerB = *((SuperpoweredAdvancedAudioPlayer **) clientData);
+        playerB->setBpm(123.0f);
+        playerB->setFirstBeatMs(40);
+        playerB->setPosition(playerB->firstBeatMs, false, false);
+    };
+}
+
+static bool audioProcessing(void *clientdata, short int *audioIO, int numberOfSamples,
+                            int __unused samplerate) {
+    return ((SuperpoweredExample *) clientdata)->process(audioIO, (unsigned int) numberOfSamples);
+}
+
+SuperpoweredExample::SuperpoweredExample(unsigned int samplerate, unsigned int buffersize,
+                                         const char *path, int fileAoffset, int fileAlength,
+                                         int fileBoffset, int fileBlength) : activeFx(0),
+                                                                             crossValue(0.0f),
+                                                                             volB(0.0f),
+                                                                             volA(1.0f * headroom) {
+    stereoBuffer = (float *) memalign(16, (buffersize + 16) * sizeof(float) * 2);
+
+    playerA = new SuperpoweredAdvancedAudioPlayer(&playerA, playerEventCallbackA, samplerate, 0);
+    playerA->open(path, fileAoffset, fileAlength);
+    playerB = new SuperpoweredAdvancedAudioPlayer(&playerB, playerEventCallbackB, samplerate, 0);
+    playerB->open(path, fileBoffset, fileBlength);
+
+    playerA->syncMode = playerB->syncMode = SuperpoweredAdvancedAudioPlayerSyncMode_TempoAndBeat;
+
+    roll = new SuperpoweredRoll(samplerate);
+    filter = new SuperpoweredFilter(SuperpoweredFilter_Resonant_Lowpass, samplerate);
+    flanger = new SuperpoweredFlanger(samplerate);
+    reverb = new SuperpoweredReverb(samplerate);
+    echo = new SuperpoweredEcho(samplerate);
+    limiter = new SuperpoweredLimiter(samplerate);
+
+    audioSystem = new SuperpoweredAndroidAudioIO(samplerate, buffersize, false, true,
+                                                 audioProcessing, this, -1, SL_ANDROID_STREAM_MEDIA,
+                                                 buffersize * 2);
+}
+
+SuperpoweredExample::~SuperpoweredExample() {
+    delete audioSystem;
+    delete playerA;
+    delete playerB;
+    free(stereoBuffer);
+}
+
+void SuperpoweredExample::onPlayPause(bool play) {
+    if (!play) {
+        playerA->pause();
+        playerB->pause();
+    } else {
+        playerA->play(true);
+        playerB->play(true);
+    };
+    SuperpoweredCPU::setSustainedPerformanceMode(play); // <-- Important to prevent audio dropouts.
+}
+
+
+void SuperpoweredExample::onFxReverbValue(int param, int value) {
+    double scaleValue = 0.01 * value;
+    __android_log_print(ANDROID_LOG_VERBOSE, "SuperpoweredExample", "Param: %i , Value: %0.2f",
+                        param, scaleValue);
+    reverb->enable(true);
+    switch (param) {
+        case REVERB_DRY:
+            reverb->setDry(scaleValue);
+            break;
+        case REVERB_MIX:
+            reverb->setMix(scaleValue);
+            break;
+        case REVERB_WIDTH:
+            reverb->setWidth(scaleValue);
+            break;
+        case REVERB_ROOMSIZE:
+            reverb->setRoomSize(scaleValue);
+            break;
+        case REVERB_WET:
+            reverb->setWet(scaleValue);
+            break;
+        case REVERB_DAMP:
+            reverb->setDamp(scaleValue);
+            break;
+    }
+
+}
+
+bool SuperpoweredExample::process(short int *output, unsigned int numberOfSamples) {
+    bool masterIsA = (crossValue <= 0.5f);
+    double masterBpm = masterIsA ? playerA->currentBpm : playerB->currentBpm;
+    double msElapsedSinceLastBeatA = playerA->msElapsedSinceLastBeat; // When playerB needs it, playerA has already stepped this value, so save it now.
+
+    bool silence = !playerA->process(stereoBuffer, false, numberOfSamples, volA, masterBpm,
+                                     playerB->msElapsedSinceLastBeat);
+    if (playerB->process(stereoBuffer, !silence, numberOfSamples, volB, masterBpm,
+                         msElapsedSinceLastBeatA))
+        silence = false;
+
+    roll->bpm = flanger->bpm = (float) masterBpm; // Syncing fx is one line.
+
+    if (roll->process(silence ? NULL : stereoBuffer, stereoBuffer, numberOfSamples) &&
+        silence)
+        silence = false;
+    if (!silence) {
+        filter->process(stereoBuffer, stereoBuffer, numberOfSamples);
+        flanger->process(stereoBuffer, stereoBuffer, numberOfSamples);
+        reverb->process(stereoBuffer, stereoBuffer, numberOfSamples);
+        echo->process(stereoBuffer, stereoBuffer, numberOfSamples);
+        limiter->process(stereoBuffer, stereoBuffer, numberOfSamples);
+    };
+
+    // The stereoBuffer is ready now, let's put the finished audio into the requested buffers.
+    if (!silence) SuperpoweredFloatToShortInt(stereoBuffer, output, numberOfSamples);
+    return !silence;
+}
+
+void SuperpoweredExample::startRecord(JNIEnv *env, jstring path) {
+    const char *pathTemp = env->GetStringUTFChars(path, false);
+    SuperpoweredRecorder *recorder = new SuperpoweredRecorder(pathTemp, 44100, 1);
+}
+
+void SuperpoweredExample::onEchoValue(int value) {
+    float scaleValue = (float) (0.01 * value);
+
+    echo->enable(true);
+    echo->setMix(scaleValue);
+}
+
+static SuperpoweredExample *example = NULL;
+
+extern "C" JNIEXPORT void
+Java_com_haudo_testsuperpoweredxx_MainActivity_SuperpoweredExample(JNIEnv *javaEnvironment,
+                                                                    jobject __unused obj,
+                                                                    jint samplerate,
+                                                                    jint buffersize,
+                                                                    jstring apkPath,
+                                                                    jint fileAoffset,
+                                                                    jint fileAlength,
+                                                                    jint fileBoffset,
+                                                                    jint fileBlength) {
+    const char *path = javaEnvironment->GetStringUTFChars(apkPath, JNI_FALSE);
+    example = new SuperpoweredExample((unsigned int) samplerate, (unsigned int) buffersize, path,
+                                      fileAoffset, fileAlength, fileBoffset, fileBlength);
+    javaEnvironment->ReleaseStringUTFChars(apkPath, path);
+}
+
+extern "C" JNIEXPORT void
+Java_com_haudo_testsuperpoweredxx_MainActivity_onPlayPause(JNIEnv *__unused javaEnvironment,
+                                                           jobject __unused obj, jboolean play) {
+    example->onPlayPause(play);
+}
+
+extern "C" JNIEXPORT void
+Java_com_haudo_testsuperpoweredxx_MainActivity_startRecord(JNIEnv *javaEnvironment,
+                                                           jobject __unused obj, jstring path) {
+    example->startRecord(javaEnvironment, path);
+}
+
+extern "C" JNIEXPORT void
+Java_com_haudo_testsuperpoweredxx_MainActivity_onFxReverbValue(JNIEnv *__unused javaEnvironment,
+                                                               jobject __unused obj, jint param,
+                                                               jint value) {
+    example->onFxReverbValue(param, value);
+}
+
+extern "C" JNIEXPORT void
+Java_com_haudo_testsuperpoweredxx_MainActivity_onEcho(JNIEnv *__unused javaEnvironment,
+                                                      jobject __unused obj,
+                                                      jint mix) {
+    example->onEchoValue(mix);
+}
